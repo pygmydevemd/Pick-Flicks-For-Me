33ff0dcd5be7d040fda3a4a1c865462b2000c862
Evan <pygmydevemd@gmail.com>
2015-01-08 19:01:42 -0800

    Fixed Margins

--- featured_movies.js
+++ featured_movies.js
@@ -1,3 +1,22 @@
-/**
- * Created by pygmydevemd on 1/2/15.
- */
+$(document).ready(function() {
+    var activeWidth = $('.poster-container').width();
+    var activeImgWidth = $('.poster-container .poster a img').width();
+
+    function balancePosters() {
+        // Calc offset for active card
+        var index = $('.poster-container').index($('.active'));
+        var offset = -1 * ((index * activeWidth) + (activeImgWidth / 2));
+        console.log($('.poster-container.active .poster img').width() / 2);
+
+        $('.poster-container.active').css({"margin-left": offset + "px"});
+    }
+
+    balancePosters();
+
+    $('.poster a').click(function (e) {
+        e.preventDefault();
+        $(this).parents('.poster-container').siblings().not('.' + $(this).attr("class")).removeClass("active").css({"margin-left": "0px"});
+        $(this).parents('.poster-container').addClass("active");
+        balancePosters();
+    });
+});
